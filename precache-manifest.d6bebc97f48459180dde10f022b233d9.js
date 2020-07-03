bef27f546d5e73a411f616e06295d4884d975052
Julian <info@racaps.com>
2020-07-03 16:52:14 -0300

    Updates

---
+++ precache-manifest.d6bebc97f48459180dde10f022b233d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85348d2fa9bd9a4b3ff07caad70257aa",
+    "url": "/formws/index.html"
+  },
+  {
+    "revision": "84b4fbf5ee29bab17d31",
+    "url": "/formws/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "06d59895a49fe7ef072a",
+    "url": "/formws/static/js/2.c9cc757f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/formws/static/js/2.c9cc757f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b4fbf5ee29bab17d31",
+    "url": "/formws/static/js/main.bf32aa26.chunk.js"
+  },
+  {
+    "revision": "ab579b0d0025eea4a148",
+    "url": "/formws/static/js/runtime-main.0dc8c57e.js"
+  }
+]);
